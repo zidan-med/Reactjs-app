9557bc731b3dff79293c368a8a7de66fb6d6362a
zidan-med <71222458+zidan-med@users.noreply.github.com>
2021-01-07 11:02:54 +0100

    react application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello all</h1>
+     <a href="" className="btn btn-primary">salam</a>
+    </div>
+  );
+}
+
+export default App;
