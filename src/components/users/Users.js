f36a8e7284872cfc2ce2d3ac9cf0007ab86358c0
zidan-med <71222458+zidan-med@users.noreply.github.com>
2021-01-12 22:17:55 +0100

    app creation

---
+++ src/components/users/Users.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+
+export class Users extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            users: [
+                {
+                    login: "mojombo",
+                    id: 1,
+                    avatar_url:"https://avatars0.githubusercontent.com/u/1?v=4",
+                    html_url:"https://github.com/mojombo",
+                    repos_url:"https://api.github.com/users/mojombo/repos"
+                },
+                {
+                    login: "defunkt",
+                    id: 2,
+                    avatar_url:"https://avatars0.githubusercontent.com/u/2?v=4",
+                    html_url:"https://github.com/defunkt",
+                    repos_url:"https://api.github.com/users/defunkt/repos"
+                },
+                {
+                    login: "pjhyett",
+                    id: 3,
+
+
+
+
+                    avatar_url:"https://avatars0.githubusercontent.com/u/3?v=4",
+                    url: "https://api.github.com/users/pjhyett",
+                    html_url:"https://github.com/pjhyett",
+                    repos_url:"https://api.github.com/users/pjhyett/repos"
+                }
+            ]
+            
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                {this.statement.users.map(user => (
+                    <div class="card">
+                        <img class="card-img-top" src="holder.js/100x180/" alt=""/>
+                        <div class="card-body">
+                            <h4 class="card-title">Title</h4>
+                            <p class="card-text">Text</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Users;
