1eec5084294497f54a4dad3aead5cfe2a242c4e6
zidan-med <71222458+zidan-med@users.noreply.github.com>
2021-02-18 13:54:11 +0100

    new version

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import Users from './components/uses/Users';
 import './App.css';
 
 function App() {
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -23,10 +23,6 @@ export class Users extends Component {
                 {
                     login: "pjhyett",
                     id: 3,
-
-
-
-
                     avatar_url:"https://avatars0.githubusercontent.com/u/3?v=4",
                     url: "https://api.github.com/users/pjhyett",
                     html_url:"https://github.com/pjhyett",
@@ -41,15 +37,23 @@ export class Users extends Component {
     render() {
         return (
             <div>
-                {this.statement.users.map(user => (
-                    <div class="card">
-                        <img class="card-img-top" src="holder.js/100x180/" alt=""/>
-                        <div class="card-body">
-                            <h4 class="card-title">Title</h4>
-                            <p class="card-text">Text</p>
+                <div className="row">
+        
+                
+                {this.state.users.map(user => (
+                    <div className="col-md-4">
+                        <div class="card">
+                            <img class="card-img-top" src={user.avatar_url} alt=""/>
+                            <div class="card-body">
+                                <h4 class="card-title">{user.login}}</h4>
+                                <p class="card-text">Text</p>
+                                    <a href={user.html_url} className="btn-success" href={user.html_url}>Show more</a>
+                                    <a href={user.repos_url} className="btn-warning" href={user.repos_url}>Repositories</a>
+                            </div>
                         </div>
                     </div>
                 ))}
+                </div>
             </div>
         )
     }
